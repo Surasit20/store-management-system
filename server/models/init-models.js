de43e7323abfe4c6ecf2d2f58b608e4d1ae713e3
Surasit20 <84635760+Surasit20@users.noreply.github.com>
2023-07-16 15:52:29 +0700

    fix bug

--- server/models/init-models.js
+++ server/models/init-models.js
@@ -1,17 +1,11 @@
 var DataTypes = require("sequelize").DataTypes;
-var _SequelizeMeta = require("./SequelizeMeta");
-var _Users = require("./Users");
 var _books_reviews = require("./books_reviews");
 
 function initModels(sequelize) {
-  var SequelizeMeta = _SequelizeMeta(sequelize, DataTypes);
-  var Users = _Users(sequelize, DataTypes);
   var books_reviews = _books_reviews(sequelize, DataTypes);
 
 
   return {
-    SequelizeMeta,
-    Users,
     books_reviews,
   };
 }
