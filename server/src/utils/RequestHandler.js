a638991b96d0572c675556804791436d3de9b616
Surasit20 <84635760+Surasit20@users.noreply.github.com>
2023-07-17 21:50:25 +0700

    fix

--- server/src/utils/RequestHandler.js
+++ server/src/utils/RequestHandler.js
@@ -1,17 +1,7 @@
-const _ = require('lodash');
-
 class RequestHandler {
-	constructor(logger) {
-		this.logger = logger;
-	}
 
 	throwIf(fn, status, errorType, errorMessage) {
 		return result => (fn(result) ? this.throwError(status, errorType, errorMessage)() : result);
-	}
-
-	validateJoi(err, status, errorType, errorMessage) {
-		if (err) { this.logger.log(`error in validating request : ${errorMessage}`, 'warn'); }
-		return !_.isNull(err) ? this.throwError(status, errorType, errorMessage)() : '';
 	}
 
 	throwError(status, errorType, errorMessage) {
@@ -29,7 +19,6 @@ class RequestHandler {
 	}
 
 	sendSuccess(res, message, status) {
-		//this.logger.log(`a request has been made and proccessed successfully at: ${new Date()}`, 'info');
 		return (data, globalData) => {
 			if (_.isUndefined(status)) {
 				status = 200;
@@ -41,7 +30,6 @@ class RequestHandler {
 	}
 
 	sendError(req, res, error) {
-		this.logger.log(`error ,Error during processing request: ${`${req.protocol}://${req.get('host')}${req.originalUrl}`} details message: ${error.message}`, 'error');
 		return res.status(error.status || 500).json({
 			type: 'error', message: error.message || error.message || 'Unhandled Error', error,
 		});
