bd80c3f21788e166e4f775f9d5d3f0138d8174ea
luck-nara <124766422+luck-nara@users.noreply.github.com>
2023-07-30 23:24:11 +0700

    page motorcycle info
    alert delete

--- client/src/admin/motorcycle_info_admin.jsx
+++ client/src/admin/motorcycle_info_admin.jsx
@@ -1,86 +1,124 @@
 import React, { Component, useEffect, useState } from "react";
-import { Form, InputGroup } from 'react-bootstrap';
+import { Form, InputGroup } from "react-bootstrap";
 import "./css/motorcycle_info.css";
 import "./css_admin.css";
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import { Navigate } from 'react-router-dom';
-import Button from '@mui/material/Button';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import { Navigate } from "react-router-dom";
+import Button from "@mui/material/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
 export default function MotorcycleInfoAdmin() {
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   const [loading, setLoading] = useState(true);
+  const [items, setItems] = useState([]);
   const handleInputChange = (e) => {
     setSearch(e.target.value);
   };
-  const [items,setItems] = useState([]);
-   const [users, setUsers] = useState([]);
-   useEffect(()=>{
-     UserGet()
-     MotorcycleGet()
-   },[]);
+  const [open, setOpen] = React.useState(false);
 
-   const MotorcycleGet = () => {
-     fetch("http://localhost:3001/api/v1/motorcycles")
-       .then(res => res.json())
-       .then((result) => {
-         if (result !== null && Array.isArray(result)) {
-           const filteredItems = result.filter((item) => item.USER_ID !== null);
-           setItems(filteredItems.map(item => {
-             const user = users.find(u => u.USER_ID === item.USER_ID);
-             return {
-               ...item,
-               USER_FULLNAME: user  // ถ้าไม่พบ user ในข้อมูลให้กำหนด USER_FULLNAME เป็น 'N/A'
-             };
-           }));
-           setLoading(false);
-         } else {
-           setItems([]);
-           setLoading(false);
-         }
-       })
-      .catch(error => console.error('Error fetching motorcycles:', error),setLoading(false));
-     }
-  
+  const handleClickOpen = (MOTORCYCLE_ID) => {
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
 
-   const UserGet = () => {
-     fetch("http://localhost:3001/api/v1/users")
-       .then(res => res.json())
-       .then((result) => {
-        const selectedFields = result.map(user => ({
+    fetch(
+      "http://localhost:3001/api/v1/motorcycles/" + MOTORCYCLE_ID,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        if (result["status"] == "ok") {
+          setOpen(true);
+        }
+      })
+      .catch((error) => console.log("error", error));
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    Promise.all([MotorcycleGet(), UserGet()])
+      .then(([motorcycles, users]) => {
+        const filteredItems = motorcycles.filter(
+          (item) => item.USER_ID !== null
+        );
+        setItems(
+          filteredItems.map((item) => {
+            const user = users.find((u) => u.USER_ID === item.USER_ID);
+            return {
+              ...item,
+              USER_FULLNAME: user ? user.USER_FULLNAME : "N/A",
+            };
+          })
+        );
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  const MotorcycleGet = () => {
+    return fetch("http://localhost:3001/api/v1/motorcycles")
+      .then((res) => res.json())
+      .catch((error) => {
+        console.error("Error fetching motorcycles:", error);
+        return [];
+      });
+  };
+
+  const UserGet = () => {
+    return fetch("http://localhost:3001/api/v1/users")
+      .then((res) => res.json())
+      .then((result) => {
+        return result.map((user) => ({
           USER_ID: user.USER_ID,
-          USER_FULLNAME: user.USER_FULLNAME
+          USER_FULLNAME: user.USER_FULLNAME,
         }));
-         setUsers(selectedFields);
-         setLoading(false);
-       })
-       .catch(error => console.error('Error fetching users:', error),setLoading(false));
-   }
+      })
+      .catch((error) => {
+        console.error("Error fetching users:", error);
+        return [];
+      });
+  };
 
-   const MotorcycleDelete = (MOTORCYCLE_ID) => {
-     var requestOptions = {
-       method: 'DELETE',
-       redirect: 'follow'
-     };
-  
-     fetch(`http://localhost:3001/api/v1/motorcycles/${MOTORCYCLE_ID}`, requestOptions)
-       .then(response => response.text())
-       .then(result => console.log(result))
-       .catch(error => console.log('error', error));
-   }
+  const MotorcycleDelete = (MOTORCYCLE_ID) => {
+    var requestOptions = {
+      method: "DELETE",
+      redirect: "follow",
+    };
 
-   const MotorcycleUpdate = (MOTORCYCLE_ID)=>{
-     window.location = '/admin/update-motorcycle/'+ MOTORCYCLE_ID
-   }
+    fetch(
+      `http://localhost:3001/api/v1/motorcycles/${MOTORCYCLE_ID}`,
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+    handleClose();
+    window.location.reload();
+  };
+
+  const MotorcycleUpdate = (MOTORCYCLE_ID) => {
+    window.location = "/admin/update-motorcycle/" + MOTORCYCLE_ID;
+  };
 
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
@@ -94,85 +132,145 @@ export default function MotorcycleInfoAdmin() {
     setPage(0);
   };
 
-  const [gotoAddMotorcycle,setGotoAddMotorcycle] = React.useState(false);
-   if(gotoAddMotorcycle){
-       return <Navigate to = "/admin/add-motorcycle" />;
-   }
-
-
-
+  const [gotoAddMotorcycle, setGotoAddMotorcycle] = React.useState(false);
+  if (gotoAddMotorcycle) {
+    return <Navigate to="/admin/add-motorcycle" />;
+  }
 
   return (
     <diV>
       <Row>
-        <Col class = "search">
-        <Form>
-      <InputGroup className="my-3">
-          <Form.Control
-            onChange={handleInputChange}
-            placeholder="ค้นหา"
-          />
-        </InputGroup>
-       </Form>
-      </Col>
-        <Col class = "additem">
-          
-        <div>
-           <button type="button" class="btn btn-danger"  onClick={()=>{setGotoAddMotorcycle(true);}}>
-        เพิ่มข้อมูล     </button></div>
-      </Col>
-      </Row> 
+        <div class="search">
+          <Col>
+            <Form>
+              <InputGroup>
+                <Form.Control
+                  onChange={handleInputChange}
+                  placeholder="ค้นหา"
+                />
+              </InputGroup>
+            </Form>
+          </Col>
+        </div>
+        <div class="additem">
+          <Col>
+            <button
+              class="btn btn-success btn-add-motor"
+              onClick={() => {
+                setGotoAddMotorcycle(true);
+              }}
+            >
+              เพิ่มข้อมูล{" "}
+            </button>
+          </Col>
+        </div>
+      </Row>
       {loading ? (
         <p>Loading...</p>
-      ) : (    
-      <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-          <TableRow>
-             <TableCell>ชื่อลูกค้า</TableCell>
-             <TableCell>เลขตัวถัง</TableCell>
-             <TableCell>เลขทะเบียน</TableCell>
-             <TableCell>แก้ไขข้อมูล</TableCell>
-             <TableCell>ลบข้อมูล</TableCell>
-           </TableRow>
-          </TableHead>
-          <TableBody>
-            {items
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .filter((row) => {
-                return search.trim() === '' || row.USER_FULLNAME.toLowerCase().includes(search.toLowerCase())
-                || row.MOTORCYCLE_BUCKET_NUMBER.toLowerCase().includes(search.toLowerCase()) 
-                || row.MOTORCYCLE_REGISTRATION_NUMBER.toLowerCase().includes(search.toLowerCase());
-              })
-              .map((row) => (
-                             <TableRow
-                               key={row.name}
-                               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                             >
-                               <TableCell >{row.USER_FULLNAME}</TableCell>
-                               <TableCell >{row.MOTORCYCLE_BUCKET_NUMBER}</TableCell>
-                               <TableCell >{row.MOTORCYCLE_REGISTRATION_NUMBER}</TableCell>
-                               <TableCell ><Button type="button" class="btn btn-warning" onClick={()=>MotorcycleUpdate(row.MOTORCYCLE_ID)}>แก้ไข</Button></TableCell>
-                               <TableCell ><Button type="button" class="btn btn-danger" onClick={()=>MotorcycleDelete(row.MOTORCYCLE_ID)}>ลบ</Button></TableCell>
-                             </TableRow>
-                           ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={items.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-          labelRowsPerPage="จำนวนแถวต่อหน้า:"
-  labelDisplayedRows={({ from, to, count }) => `${from}-${to} จากทั้งหมด ${count}`}
-      />
-    </Paper>)}
+      ) : (
+        <Paper sx={{ width: "100%", overflow: "hidden" }}>
+          <TableContainer sx={{ maxHeight: 440 }}>
+            <Table stickyHeader aria-label="sticky table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>ชื่อลูกค้า</TableCell>
+                  <TableCell>เลขตัวถัง</TableCell>
+                  <TableCell>เลขทะเบียน</TableCell>
+                  <TableCell>แก้ไขข้อมูล</TableCell>
+                  <TableCell>ลบข้อมูล</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {items
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .filter((row) => {
+                    return (
+                      search.trim() === "" ||
+                      row.USER_FULLNAME.toLowerCase().includes(
+                        search.toLowerCase()
+                      ) ||
+                      row.MOTORCYCLE_BUCKET_NUMBER.toLowerCase().includes(
+                        search.toLowerCase()
+                      ) ||
+                      row.MOTORCYCLE_REGISTRATION_NUMBER.toLowerCase().includes(
+                        search.toLowerCase()
+                      )
+                    );
+                  })
+                  .map((row) => (
+                    <TableRow
+                      key={row.name}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell>{row.USER_FULLNAME}</TableCell>
+                      <TableCell>{row.MOTORCYCLE_BUCKET_NUMBER}</TableCell>
+                      <TableCell>
+                        {row.MOTORCYCLE_REGISTRATION_NUMBER}
+                      </TableCell>
+                      <TableCell>
+                        <Button
+                          type="button"
+                          class="btn btn-warning"
+                          onClick={() => MotorcycleUpdate(row.MOTORCYCLE_ID)}
+                        >
+                          แก้ไข
+                        </Button>
+                      </TableCell>
+                      <Dialog
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="alert-dialog-title"
+                        aria-describedby="alert-dialog-description"
+                      >
+                        <DialogTitle id="alert-dialog-title">
+                          {"คุณต้องการลบข้อมูลนี้ใช่หรือไม่?"}
+                        </DialogTitle>
+                        <DialogContent>
+                          <DialogContentText id="alert-dialog-description">
+                            คุณต้องการลบข้อมูลนี้ใช่หรือไม่?
+                            {/* คุณสามารถแสดงข้อความตามต้องการในส่วนนี้ */}
+                          </DialogContentText>
+                        </DialogContent>
+                        <DialogActions>
+                          <Button
+                            onClick={() => MotorcycleDelete(row.MOTORCYCLE_ID)}
+                          >
+                            ยืนยัน
+                          </Button>
+                          <Button onClick={handleClose} autoFocus>
+                            ยกเลิก
+                          </Button>
+                        </DialogActions>
+                      </Dialog>
+                      <TableCell>
+                        <Button
+                          type="button"
+                          class="btn btn-danger"
+                          onClick={() => handleClickOpen(row.MOTORCYCLE_ID)}
+                        >
+                          ลบ
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[10, 25, 100]}
+            component="div"
+            count={items.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+            labelRowsPerPage="จำนวนแถวต่อหน้า:"
+            labelDisplayedRows={({ from, to, count }) =>
+              `${from}-${to} จากทั้งหมด ${count}`
+            }
+          />
+        </Paper>
+      )}
     </diV>
-    
   );
 }
--- client/src/admin/motorcycle_update.jsx
+++ client/src/admin/motorcycle_update.jsx
@@ -1,150 +1,188 @@
 import React, { Component, useState, useEffect } from "react";
+import "../admin/css/motorcycle_update.css";
 import { useParams } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import { withRouter } from "react-router";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import { Navigate } from 'react-router-dom';
+import { Navigate } from "react-router-dom";
 import { Grid, TextField } from "@mui/material";
-
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 function UpdateMotorcycle() {
-  const {MOTORCYCLE_ID} = useParams();
+  const { MOTORCYCLE_ID } = useParams();
+  const navigate = useNavigate();
 
-  useEffect(()=>{
-  var requestOptions = {
-  method: 'GET',
-  redirect: 'follow'
+  useEffect(() => {
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "http://localhost:3001/api/v1/motorcycles/" + MOTORCYCLE_ID,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        if (result["status"] == "ok") {
+          setRegistrationNumber(
+            result["result"]["MOTORCYCLE_REGISTRATION_NUMBER"]
+          );
+          setBucketNumber(result["result"]["MOTORCYCLE_BUCKET_NUMBER"]);
+          setBrand(result["result"]["MOTORCYCLE_BRAND"]);
+          setModel(result["result"]["MOTORCYCLE_MODEL"]);
+          setColor(result["result"]["MOTORCYCLE_COLOR"]);
+          setPrice(result["result"]["MOTORCYCLE_PRICE"]);
+          setBalance(result["result"]["MOTORCYCLE_BALANCE"]);
+        }
+      })
+      .catch((error) => console.log("error", error));
+  }, [MOTORCYCLE_ID]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      MOTORCYCLE_BALANCE: Balance,
+      MOTORCYCLE_PRICE: Price,
+      MOTORCYCLE_BRAND: Brand,
+      MOTORCYCLE_MODEL: Model,
+      MOTORCYCLE_COLOR: Color,
+      MOTORCYCLE_REGISTRATION_NUMBER: RegistrationNumber,
+      MOTORCYCLE_BUCKET_NUMBER: BucketNumber,
+    });
+
+    var requestOptions = {
+      method: "PUT",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch(
+      `http://localhost:3001/api/v1/motorcycles/${MOTORCYCLE_ID}`,
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+      navigate("/admin/motorcycle");
   };
 
-  fetch("http://localhost:3001/api/v1/motorcycles/"+ MOTORCYCLE_ID, requestOptions)
-  .then(response => response.json())
-  .then(result => {
-    if(result['status']=='ok'){
-      setRegistrationNumber(result['result']['MOTORCYCLE_REGISTRATION_NUMBER'])
-      setBucketNumber(result['result']['MOTORCYCLE_BUCKET_NUMBER'])
-      setBrand(result['result']['MOTORCYCLE_BRAND'])
-      setModel(result['result']['MOTORCYCLE_MODEL'])
-      setColor(result['result']['MOTORCYCLE_COLOR'])
-      setPrice(result['result']['MOTORCYCLE_PRICE'])
-      setBalance(result['result']['MOTORCYCLE_BALANCE'])
-    }
-  })
-  .catch(error => console.log('error', error));
-  },[MOTORCYCLE_ID])
+  const [RegistrationNumber, setRegistrationNumber] = useState("");
+  const [BucketNumber, setBucketNumber] = useState("");
+  const [Brand, setBrand] = useState("");
+  const [Model, setModel] = useState("");
+  const [Color, setColor] = useState("");
+  const [Price, setPrice] = useState("");
+  const [Balance, setBalance] = useState("");
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div class="row">
+          <Row>
+            <div class="container-textfile">
+              <Col>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    id="RegistrationNumber"
+                    label="เลขทะเบียน"
+                    variant="outlined"
+                    fullWidth
+                    required
+                    onChange={(e) => setRegistrationNumber(e.target.value)}
+                    value={RegistrationNumber}
+                  ></TextField>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    id="BucketNumber"
+                    label="เลขตัวถัง"
+                    variant="outlined"
+                    fullWidth
+                    required
+                    onChange={(e) => setBucketNumber(e.target.value)}
+                    value={BucketNumber}
+                  ></TextField>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    id="Brand"
+                    label="ยี่ห้อ"
+                    variant="outlined"
+                    fullWidth
+                    required
+                    onChange={(e) => setBrand(e.target.value)}
+                    value={Brand}
+                  ></TextField>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    id="Model"
+                    label="รุ่น"
+                    variant="outlined"
+                    fullWidth
+                    required
+                    onChange={(e) => setModel(e.target.value)}
+                    value={Model}
+                  ></TextField>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    id="Color"
+                    label="สี"
+                    variant="outlined"
+                    fullWidth
+                    required
+                    onChange={(e) => setColor(e.target.value)}
+                    value={Color}
+                  ></TextField>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    id="Price"
+                    label="ราคา"
+                    variant="outlined"
+                    fullWidth
+                    required
+                    onChange={(e) => setPrice(e.target.value)}
+                    value={Price}
+                  ></TextField>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    id="Balance"
+                    label="ยอดคงเหลือ"
+                    variant="outlined"
+                    fullWidth
+                    required
+                    onChange={(e) => setBalance(e.target.value)}
+                    value={Balance}
+                  ></TextField>
+                </Grid>
+              </Col>
+            </div>
+            <div className="container-image">
+              <Col>
+                ใส่รูป
+              </Col>
+            </div>
+          </Row>
+        </div>
 
-    const handleSubmit = event =>{
-        event.preventDefault();
-        var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
-
-var raw = JSON.stringify({
-  "MOTORCYCLE_BALANCE": Balance,
-  "MOTORCYCLE_PRICE": Price,
-  "MOTORCYCLE_BRAND": Brand,
-  "MOTORCYCLE_MODEL": Model,
-  "MOTORCYCLE_COLOR": Color,
-  "MOTORCYCLE_REGISTRATION_NUMBER": RegistrationNumber,
-  "MOTORCYCLE_BUCKET_NUMBER": BucketNumber
-});
-
-var requestOptions = {
-  method: 'PUT',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
-};
-
-fetch(`http://localhost:3001/api/v1/motorcycles/${MOTORCYCLE_ID}`, requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-    }
-
-    const[RegistrationNumber,setRegistrationNumber] = useState('');
-    const[BucketNumber,setBucketNumber] = useState('');
-    const[Brand,setBrand] = useState('');
-    const[Model,setModel] = useState('');
-    const[Color,setColor] = useState('');
-    const[Price,setPrice] = useState('');
-    const[Balance,setBalance] = useState('');
-    return(       
-        <div>
-            <form onSubmit={handleSubmit}>
-                <Grid item xs ={12} sm = {6}>
-                    <TextField 
-                    id="RegistrationNumber" 
-                    label="เลขทะเบียน" 
-                    variant="outlined" 
-                    fullWidth required
-                    onChange={(e)=>setRegistrationNumber(e.target.value)}
-                    value={RegistrationNumber}>
-                     </TextField>
-                </Grid>
-                <Grid item xs ={12} sm = {6}>
-                    <TextField 
-                    id="BucketNumber" 
-                    label="เลขตัวถัง" 
-                    variant="outlined" 
-                    fullWidth required
-                    onChange={(e)=>setBucketNumber(e.target.value)}
-                    value={BucketNumber}>
-                     </TextField>
-                </Grid>
-                <Grid item xs ={12} sm = {6}>
-                    <TextField 
-                    id="Brand" 
-                    label="ยี่ห้อ" 
-                    variant="outlined" 
-                    fullWidth required
-                    onChange={(e)=>setBrand(e.target.value)}
-                    value={Brand}>
-                     </TextField>
-                </Grid>
-                <Grid item xs ={12} sm = {6}>
-                    <TextField 
-                    id="Model" 
-                    label="รุ่น" 
-                    variant="outlined" 
-                    fullWidth required
-                    onChange={(e)=>setModel(e.target.value)}
-                    value={Model}>
-                     </TextField>
-                </Grid>
-                <Grid item xs ={12} sm = {6}>
-                    <TextField 
-                    id="Color" 
-                    label="สี" 
-                    variant="outlined" 
-                    fullWidth required
-                    onChange={(e)=>setColor(e.target.value)} 
-                    value={Color}>
-                     </TextField>
-                </Grid>
-                <Grid item xs ={12} sm = {6}>
-                    <TextField 
-                    id="Price" 
-                    label="ราคา" 
-                    variant="outlined" 
-                    fullWidth required
-                    onChange={(e)=>setPrice(e.target.value)}
-                    value={Price}>
-                     </TextField>
-                </Grid>
-                <Grid item xs ={12} sm = {6}>
-                    <TextField 
-                    id="Balance" 
-                    label="ยอดคงเหลือ" 
-                    variant="outlined" 
-                    fullWidth required
-                    onChange={(e)=>setBalance(e.target.value)}
-                    value={Balance}>
-                     </TextField>
-                </Grid> 
-                <button type="submit" variant="contained" class="btn btn-primary mb-3">
-            แก้ไข
-          </button>
-            </form>
-       
+        <button type="submit" variant="contained" class="btn btn-primary mb-3">
+          บันทึก
+        </button>
+      </form>
     </div>
-    );
+  );
 }
 export default UpdateMotorcycle;
